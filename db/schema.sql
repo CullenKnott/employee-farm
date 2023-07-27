735808ee6f8a62fbf9d049dc7015d5b475001c52
Cullen Knott <cullenp.knott@gmail.com>
2023-07-27 15:06:58 -0700

    feature to add roles to database

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    employee_name VARCHAR(100) NOT NULL
+    department_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE role (
@@ -20,13 +20,10 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
     manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function prompt() {
           addRole();
           break;
         case "Add an employee":
-          addemployee();
+          addEmployee();
           break;
         case "Update employee":
           updateEmployee();
@@ -65,9 +65,12 @@ function prompt() {
 }
 
 function viewAllEmployees() {
-  db.query("SELECT * FROM employee", function (err, res) {
-    err ? console.log(err) : console.table(res), prompt();
-  });
+  db.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.department_name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      err ? console.log(err) : console.table(res), prompt();
+    }
+  );
 }
 
 function viewAllDepartments() {
@@ -82,4 +85,123 @@ function viewAllRoles() {
   });
 }
 
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addDepartment",
+        message: "Enter a new department.",
+      },
+    ])
+    .then((answer) => {
+      let departmentName = answer.addDepartment;
+      db.query(
+        `INSERT INTO department (department_name) VALUES ('${departmentName}')`,
+        function (err, res) {
+          err ? console.log(err) : viewAllDepartments(), prompt();
+        }
+      );
+    });
+}
+
+function addRole() {
+  db.query("SELECT * FROM department", function (err, res) {
+    if (err) {
+      console.log(err);
+      return prompt();
+    }
+    const departmentChoices = res.map((department) => ({
+      value: department.id,
+      name: department.department_name,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "newRole",
+          message: "Write the name of the new role you would like to add.",
+        },
+        {
+          type: "input",
+          name: "newSalary",
+          message: "Input a salary for this new role (must be an interger).",
+        },
+        {
+          type: "list",
+          name: "dept",
+          message: "Add the new role to a department",
+          choices: departmentChoices,
+        },
+      ])
+      .then((inquirerResponse) => {
+        console.log("Role added: " + inquirerResponse.newRole);
+        let deptId = inquirerResponse.dept;
+        let roleName = inquirerResponse.newRole;
+        let roleSalary = inquirerResponse.newSalary;
+        db.query(
+          `INSERT INTO role
+      (title, salary, department_id)
+      VALUES
+      ('${roleName}',
+      '${roleSalary}',
+      '${deptId}')`,
+          function (err, res) {
+            err ? console.log(err) : viewAllRoles(), prompt();
+          }
+        );
+      });
+  });
+}
+
+function addEmployee() {}
+
+// 2 more add functions
+// update function
+
 prompt();
+
+// function addARole() {
+//   db.query("SELECT * FROM department_list", function(err, results) {
+//     if (err) {
+//       console.log(err);
+//       return workTime();
+//     }
+//     const departmentChoices = results.map(department => ({
+//       value: department.id,
+//       name: department.dept_name
+//     }));
+//     inquirer.prompt([
+//       {
+//         type: "input",
+//         name: "addARole",
+//         message: "Enter a Role Dood."
+//       },
+//       {
+//         type: "input",
+//         name: "salary",
+//         message: "how much this joker making?"
+//       },
+//       {
+//         type: "list",
+//         name: "deptId",
+//         message: "witch department does this belong to?",
+//         choices: departmentChoices
+//       }
+// ]).then((inquirerResponse)=> {
+//     console.log("Role added:  " + inquirerResponse.addARole)
+//     let departmentId = inquirerResponse.deptId;
+//     let roleName = inquirerResponse.addARole;
+//     let roleSalary = inquirerResponse.salary;
+//     db.query(`INSERT INTO
+//              role_list
+//              (title, salary, department_list_id)
+//              VALUES
+//              ('${roleName}',
+//             '${roleSalary}',
+//             '${departmentId}')`, function(err, results){
+//       (err) ? console.log(err) : console.table(`Added:  ${roleName}!!!!`,results) , workTime()
+//     })
+//   })
+// });
